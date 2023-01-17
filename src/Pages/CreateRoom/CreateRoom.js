d91c6eba81ea4f2b08e319ca2369abdfa5058109
mitulchy07 <100080242+mitulchy07@users.noreply.github.com>
2023-01-17 23:25:10 +0100

    page title dynamically change

--- src/Pages/CreateRoom/CreateRoom.js
+++ src/Pages/CreateRoom/CreateRoom.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const CreateRoom = () => {
+  const [page, setPage] = useState(0);
+  const formTitle = [
+    'Number of the people in the room',
+    'Set Room Dimention',
+    'Set Display System',
+    'Set Lighting System',
+    'Set Audio System',
+  ];
   return (
-    <div>
-      <div>Body</div>
+    <div className='center grid gap-5 justify-center content-center my-5'>
+      <div>
+        <h1 className='text-3xl'>{formTitle[page]}:</h1>
+      </div>
+      <div className='flex gap-5'>
+        <Link className='btn btn-accent'>Prev</Link>
+        <Link className='btn btn-secondary'>Next</Link>
+      </div>
     </div>
   );
 };
